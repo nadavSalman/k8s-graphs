5599b43da6c74b4c9f931999a83e8b4d89514cba
Nadav Salman <v-nsalman@microsoft.com>
2024-05-24 10:39:33 +0300

    Extract pods graph node metadada & volums for secrets and configmaps mapping

--- graph-data-extraction/main.py
+++ graph-data-extraction/main.py
@@ -3,29 +3,41 @@ import json
 import pprint as pp
 
 
+
+
+def extractedpods_data(k8s_client: client.CoreV1Api):
+    all_namespases_pods: list = k8s_client.list_pod_for_all_namespaces().to_dict().get('items')
+    all_pods_extract_data = []
+    for pod in all_namespases_pods:
+        pod_volumes = []
+        
+        # crean not extis volumes
+        for volume in pod['spec']['volumes']:
+            instance_volume = {
+                'name': volume['name']
+            }
+            for key, value in volume.items():
+                if value is not None:
+                    instance_volume[key] = value
+            pod_volumes.append(instance_volume)
+            
+        all_pods_extract_data.append( {
+            'name': pod['metadata']['name'],
+            'namespace': pod['metadata']['namespace'],
+            'labels': pod['metadata']['labels'],
+            'volume': pod_volumes,
+        })
+    return all_pods_extract_data
+        
+
 def main():
     # Configs can be set in Configuration class directly or using helper utility
     config.load_kube_config()
-
     k8s_client = client.CoreV1Api()
         
-    api_resources = k8s_client.get_api_resources()
-    # print(type(api_resources))
-    # print(dir(api_resources))
-
-    # print(type(api_resources.resources))
-   
-    resources_list = [(resource.kind,resource.name) for resource in   api_resources.resources]
+    all_pods_extract_data = extractedpods_data(k8s_client)
+    pp.pprint(all_pods_extract_data[-1])
     
     
-    pp.pprint(resources_list)
-    print(f"{ len(resources_list) = }")
-        
-    for item in api_resources.resources:
-        if item.kind == "Namespace":
-            print(item)
-    
-    
-
 if __name__ == '__main__':
     main()
